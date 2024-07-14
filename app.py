7b4ad9aacaf01c0239f6c3e7953a007a894b704d
Ramu18github <96322276+Ramu18github@users.noreply.github.com>
2024-07-14 13:47:57 +0000

    add data

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-  return "Hello, Flask!"
+  return render_template('home.html')
+
 
 if __name__ == '__main__':
-  app.run(host ='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0', debug=True)
